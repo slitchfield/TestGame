00bc4e06ca1e41e500d4d8dccfbecdfb408f84db
Samuel Litchfield <slitchfield3@gatech.edu>
2014-09-11 12:22:42 -0400

    Added main too.

---
+++ TextGame/main.cpp
@@ -0,0 +1,15 @@
+/* Text Game
+ * author: Samuel Litchfield
+ *
+ * main version 0.0001
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv){
+
+  std::cout << "/**************************\\" << std::endl;
+  std::cout << "| THIS IS A TEXT GAMEEEEEE |" << std::endl;
+  std::cout << "\\**************************/" << std::endl;
+
+}
